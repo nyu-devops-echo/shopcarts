0efe014119c4d0a16b81b3c30adf7cb3398dffa8
Radhika Mattoo <rm3485@nyu.edu>
2017-10-02 14:27:03 -0400

    added code for python invocation

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
+import os
 from flask import Flask
 app = Flask(__name__)
+
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+PORT = os.getenv('PORT', '5000')
 
 @app.route("/")
 def home():
     return "Home"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
