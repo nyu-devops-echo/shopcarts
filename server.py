06c247a84dbe18120d7bf65feaa5c6e9725f2d88
Radhika Mattoo <rm3485@nyu.edu>
2017-10-14 19:53:19 -0400

    view shopcart code

--- server.py
+++ server.py
@@ -1,11 +1,20 @@
 import os
 from flask import Flask, jsonify
 from flask_api import status
+from models.shopcart import Shopcart
 
 app = Flask(__name__)
 
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 PORT = os.getenv('PORT', '5000')
+
+# Status Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
+HTTP_400_BAD_REQUEST = 400
+HTTP_404_NOT_FOUND = 404
+HTTP_409_CONFLICT = 409
 
 ######################################################################
 # GET INDEX
@@ -17,5 +26,22 @@ def index():
                    version='1.0',
                    description= 'This is the REST API Service for the shopcarts.'), status.HTTP_200_OK
 
+######################################################################
+# RETRIEVE A SHOPCART
+######################################################################
+@app.route('/shopcarts/<int:id>', methods=['GET'])
+def get_shopcarts(id):
+    cart = Shopcart.find(id)
+    if cart:
+        message = { 'uid': cart.uid, 'products': cart.products }
+        rc = HTTP_200_OK
+    else:
+        message = { 'error' : 'Shopcart with id: %s was not found' % str(id) }
+        rc = HTTP_404_NOT_FOUND
+
+    return jsonify(message), rc
+
+
 if __name__ == "__main__":
+    # dummy data for server testing
     app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
--- tests/test_server.py
+++ tests/test_server.py
@@ -17,6 +17,27 @@ class TestServer(unittest.TestCase):
         self.assertEqual(data['description'], 'This is the REST API Service for the shopcarts.')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
-        
+    def test_get_shopcart(self):
+        """ Test Get a Shopcart """
+        # Test for existing shopcarts
+        resp = self.app.get('/shopcarts/1')
+        self.assertEqual( resp.status_code, HTTP_200_OK )
+        data = json.loads(resp.data.decode('utf8'))
+        self.assertEqual (data['uid'], 1)
+        self.assertEqual (data['products'], {})
+
+        resp = self.app.get('/shopcarts/2')
+        self.assertEqual( resp.status_code, HTTP_200_OK )
+        data = json.loads(resp.data.decode('utf8'))
+        self.assertEqual (data['uid'], 2)
+        self.assertEqual (data['products'], {})
+
+        # Test for non-existent shopcart
+        resp = self.app.get('/shopcarts/3')
+        self.assertEqual( resp.status_code, HTTP_404_NOT_FOUND )
+        data = json.loads(resp.data.decode('utf8'))
+        self.assertEqual (data['error'], 'Shopcart with id: 3 was not found')
+
+
 if __name__ == '__main__':
     unittest.main()
