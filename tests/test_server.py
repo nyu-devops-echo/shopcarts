80e5d80e130d06675dfbd397d5eef387f5a70480
Carlos Petricioli <carpetri@gmail.com>
2017-10-14 00:57:05 -0400

    Adding basic description to the root

--- tests/test_server.py
+++ tests/test_server.py
@@ -12,11 +12,11 @@ class TestServer(unittest.TestCase):
     def test_index(self):
         """ Test the Home Page """
         resp = self.app.get('/')
-
         data = json.loads(resp.data.decode('utf8'))
-
+        self.assertEqual(data['name'], 'Shopcarts REST API Service')
+        self.assertEqual(data['description'], 'This is the REST API Service for the shopcarts.')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        self.assertEqual(data, 'Home')
 
+        
 if __name__ == '__main__':
     unittest.main()
