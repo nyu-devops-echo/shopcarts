4b39900ba41938a2ffc5c57bd64bc22c84ea874e
Carlos Petricioli <carpetri@gmail.com>
2017-10-13 22:45:46 -0400

    Added method for adding a product to a shopcart

--- models/shopcart.py
+++ models/shopcart.py
@@ -1,34 +1,32 @@
+from .dataerror import DataValidationError
 class Shopcart(object):
-
     """
     This is model for the shop carts
     Assumptions:
         - In memory persistence
         - The fields of a shopcart include:
             - user id
-            - list of products (product id, quantity of product )
+            - Dictionary of products { product_id: quantity_of_product}
         - The total price of the shopcart will be dynamically calculated
-        - Model should only initially include `init` method and shopcart fields
-
     """
-
     __data = []
     __index = 0
-
-    __products = dict()
 
     def __init__(self, uid=0, products=None):
         """
         :param uid: user id
         :param products: dict of products <products id, quantity of product>
         """
-
         self.uid = int(uid)
-        self.products = products if products else []
+        self.products = self.__validate_products(products)
 
     def save(self):
         """ Saves a Shopcart in the database """
         Shopcart.__data.append(self)
+
+    def add_product(self, pq_tup):
+        """ Adds a tuple of product, quantity to the product dict """
+        self.products.update( self.__validate_products(pq_tup) )
 
     @staticmethod
     def all():
@@ -39,3 +37,36 @@ class Shopcart(object):
     def remove_all():
         """ Remove all Shopcarts from memory """
         Shopcart.__data = []
+
+    @staticmethod
+    def __validate_products(products):
+        """ Validates products or raises an error"""
+        try:
+            # Product is none so set an empty list
+            if products is None:
+                return {}
+            
+            #Not None
+            # Tuple of product, quantity 
+            if type(products) == tuple  and len(products)==2 and all(isinstance(pq,int) for pq in products): 
+                return {products[0]:products[1]}
+
+            # Just a Product id, set default quantity to 1
+            if type(products) == int: 
+                return {products:1}
+
+            if type(products) != dict : 
+                raise DataValidationError() 
+            
+            # Products is a dict of proper tuples
+            if ( all( isinstance(pid,int) for pid in products.keys() ) and
+                 all( (isinstance(q,int) and (q > 0)) for q in products.values() ) ):
+                return products
+
+            #Products not valid
+            raise DataValidationError() 
+
+        except DataValidationError as e:
+            print(type(e)) 
+            print('ERROR: Products were not added to shopcart.')
+            return {}
