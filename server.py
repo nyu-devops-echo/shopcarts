74a4acdcbc318ad32b5fbf22b092d41da2790720
Carlos Petricioli <carpetri@gmail.com>
2017-10-14 20:14:24 -0400

    think I fixed the mess

--- server.py
+++ server.py
@@ -25,40 +25,7 @@ def index():
                    version='1.0',
                    description= 'This is the REST API Service for the shopcarts.'), status.HTTP_200_OK
 
-######################################################################
-<<<<<<< HEAD
-# RETRIEVE A Shopcart 
-######################################################################
-@app.route('/shopcarts/<int:user_id>', methods=['GET'])
-def get_shopcarts(uid):
-    """
-    Retrieve a single shopcart
-    This endpoint will return a shopcart based on it's user id
-    """
-    shopcart = Shopcart.find(uid)
-    if not shopcart:
-        raise Exception("Shopcart with id '{}' was not found.".format(uid))
-    return make_response(jsonify(shopcart.serialize()), status.HTTP_200_OK)
 
-######################################################################
-# ADD A NEW CART
-######################################################################
-@app.route('/shopcarts', methods=['POST'])
-def create_shopcart():
-    """
-    Creates a shopcart
-    This endpoint will create a shopcart based the data in the body that is posted
-    """
-    shopcart = Shopcart()
-    shopcart.deserialize(request.get_json())
-    shopcart.save()
-    message = shopcart.serialize()
-    location_url = url_for('get_shopcarts', shopcart_id=shopcart.uid, _external=True)
-    return make_response(jsonify(message), status.HTTP_201_CREATED,
-                         {
-                             'Location': location_url
-                         })
-=======
 # RETRIEVE A SHOPCART
 ######################################################################
 @app.route('/shopcarts/<int:id>', methods=['GET'])
@@ -73,7 +40,6 @@ def get_shopcarts(id):
 
     return jsonify(message), rc
 
->>>>>>> 353c5ac910ecc67f05dba2b9f17c8ad039f1d23e
 
 if __name__ == "__main__":
     # dummy data for server testing
