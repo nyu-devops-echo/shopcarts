75cfddbd44d66a6c121e91a4fda754052a4a86a8
RegulusXiang <regulusxg.aiesec@gmail.com>
2017-11-13 16:18:16 -0500

    Check if the product exists before add to the Product table.

--- server.py
+++ server.py
@@ -230,9 +230,20 @@ def init_db():
     p1 = Product(id=1,name='Apple',price=1.2,description='Fruit')
     p2 = Product(id=2,name='Pen',price=3.4,description='Stationery')
     p3 = Product(id=3,name='Pineapple',price=2.3,description='Fruit')
-    db.session.add(p1)
-    db.session.add(p2)
-    db.session.add(p3)
+    p4 = Product(id=4,name='Beef',price=33.0,description='Meat')
+    p5 = Product(id=5,name='Notebook',price=0.99,description='Stationery')
+    
+    # check exist before add
+    if db.session.query(Product.id).filter(Product.id==1).count() == 0:
+        db.session.add(p1)
+    if db.session.query(Product.id).filter(Product.id==2).count() == 0:
+        db.session.add(p2)
+    if db.session.query(Product.id).filter(Product.id==3).count() == 0:
+        db.session.add(p3)
+    if db.session.query(Product.id).filter(Product.id==4).count() == 0:
+        db.session.add(p4)
+    if db.session.query(Product.id).filter(Product.id==5).count() == 0:
+        db.session.add(p5)
     db.session.commit()
 
 ######################################################################
