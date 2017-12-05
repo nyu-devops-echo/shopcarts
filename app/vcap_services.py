da71645526165f9c1f19bc5a24ece7b6c0256266
Carlos Petricioli <carpetri@gmail.com>
2017-12-04 19:56:58 -0500

    back to long if haha it was not working

--- app/vcap_services.py
+++ app/vcap_services.py
@@ -40,6 +40,12 @@ def get_database_uri():
         hostname = creds["hostname"]
         port = creds["port"]
         name = creds["name"]
+    elif 'TRAVIS' in os.environ:
+        username = 'root'
+        password = ''
+        hostname = 'localhost'
+        port = '3306'
+        name = 'shopcarts'
     else:
         username = 'root'
         password = 'root'
@@ -47,8 +53,5 @@ def get_database_uri():
         port = '3306'
         name = 'shopcarts'
 
-    if 'TRAVIS' in os.environ:
-        password = ''
-    
     connect_string = 'mysql+pymysql://{}:{}@{}:{}/{}'
     return connect_string.format(username, password, hostname, port, name)
