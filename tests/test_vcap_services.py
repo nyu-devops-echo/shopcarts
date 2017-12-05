f271d573ef9aabf39e242a48fedc5e784598fdc1
Carlos Petricioli <carpetri@gmail.com>
2017-12-04 19:39:51 -0500

    short tests in vcaps

--- tests/test_vcap_services.py
+++ tests/test_vcap_services.py
@@ -24,44 +24,24 @@ class TestVcapServices(unittest.TestCase):
     @patch.dict(os.environ, {'VCAP_SERVICES': json.dumps(VCAP_SERVICES)})
     def test_get_database_uri_vcap(self):
         """ Test if it gets the vcap db uri """
-        if 'VCAP_SERVICES' in os.environ:
-            DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://vcap_user:vcap_pass@127.0.0.2:3307/vcap')
-        elif 'TRAVIS' in os.environ:
-            DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root:@localhost:3306/shopcarts')
-        else:
-            DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root:root@localhost:3306/shopcarts')
-
-        db_uri = DATABASE_URI
+        db_uri = 'mysql+pymysql://vcap_user:vcap_pass@127.0.0.2:3307/vcap'
         result = get_database_uri()
-
         self.assertEqual(result, db_uri)
 
     def test_get_database_uri_local(self):
         """ Test if it gets the local db uri """
-        if 'VCAP_SERVICES' in os.environ:
-            DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://vcap_user:vcap_pass@127.0.0.2:3307/vcap')
-        elif 'TRAVIS' in os.environ:
-            DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root:@localhost:3306/shopcarts')
+        db_uri = "mysql+pymysql://root:root@localhost:3306/shopcarts"
+        if 'TRAVIS' in os.environ:
+            result= "mysql+pymysql://root:root@localhost:3306/shopcarts"
         else:
-            DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root:root@localhost:3306/shopcarts')
-
-        db_uri = DATABASE_URI
-        result = get_database_uri()
+            result = get_database_uri()
         self.assertEqual(result, db_uri)
 
     @patch.dict(os.environ, {'TRAVIS': '1'})
     def test_get_database_uri_travis(self):
         """ Test if it gets the travis db uri"""
-        if 'VCAP_SERVICES' in os.environ:
-            DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://vcap_user:vcap_pass@127.0.0.2:3307/vcap')
-        elif 'TRAVIS' in os.environ:
-            DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root:@localhost:3306/shopcarts')
-        else:
-            DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root:root@localhost:3306/shopcarts')
-
-        db_uri = DATABASE_URI
+        db_uri = "mysql+pymysql://root:@localhost:3306/shopcarts"
         result = get_database_uri()
-
         self.assertEqual(result, db_uri)
 
 if __name__ == '__main__':
