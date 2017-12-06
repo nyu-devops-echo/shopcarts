c492495119e7f9fb892b96c558a1d89e4b7b2d0e
Carlos Petricioli <carpetri@gmail.com>
2017-12-05 23:36:27 -0500

    setup flassger (#104)
    * setup flassger
    
    * removed misplaced description from the root page
    
    * correct path to specs

--- app/__init__.py
+++ app/__init__.py
@@ -15,3 +15,21 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import server, models
+from flasgger import Swagger
+
+app.config['SWAGGER'] = {
+    "swagger_version": "2.0",
+    "specs": [
+        {
+            "version": "1.0.0",
+            "title": "Shopcarts REST API Service",
+            "description":"This is the Shopcart API",
+            "endpoint": 'v1_spec',
+            "route": '/v1/spec',
+            "basePath": '/'
+        }
+    ],
+}
+
+# Initialize Swagger after configuring it
+Swagger(app)
